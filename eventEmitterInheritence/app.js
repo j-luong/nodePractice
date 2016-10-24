0bf8dfe95b026c3a2776efbfa77b9a620fbad924
Jason Luong <jasonluong@Jasons-MacBook-Pro.local>
2016-10-24 16:34:11 +0100

    added eventEmitterInheritence

---
+++ eventEmitterInheritence/app.js
@@ -0,0 +1,25 @@
+var EventEmitter = require('events');
+var util = require('util');
+var eventConfig = require('./config');
+
+function Greetr() {
+  this.greeting = "Hello world!";
+}
+
+// inherit props of EventEmitter into Greetr
+util.inherits(Greetr, EventEmitter);
+
+Greetr.prototype.greet = function(data) {
+  console.log(this.greeting + ': ' + data);
+  this.emit(eventConfig.GREET, data);
+}
+
+var greeter1 = new Greetr();
+
+// Greetr is able to use '.on', which is a prop of EventEmitter
+// this is because of line 10
+greeter1.on(eventConfig.GREET, function(data) {
+  console.log("Someone greeted!: " + data);
+})
+
+greeter1.greet("Jason");
